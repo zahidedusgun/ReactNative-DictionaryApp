8ea7ebea60937309d610b95d34aed71b088c2dd7
zahidsgn <zahide.dusgun@gmail.com>
2023-12-11 14:08:53 +0300

    Speech function added and frontend design edited

--- App.js
+++ App.js
@@ -9,11 +9,14 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
   const [word, setWord] = React.useState("");
   const [checkedWord, setCheckedWord] = React.useState("");
   const [definition, setDefinition] = React.useState("");
+  const [phonetic, setPhonetic] = React.useState("");
+  const [kind, setKind] = React.useState("");
 
   const searchWord = (getWord) => {
     setWord(getWord);
@@ -22,7 +25,7 @@ export default function App() {
 
   requestAPI = () => {
     var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-  
+
     fetch(url)
       .then((data) => {
         return data.json();
@@ -33,14 +36,30 @@ export default function App() {
 
         var definition = response[0].meanings[0].definitions[0].definition;
         setDefinition(definition);
+
+        var phonetic = response[0].phonetic;
+        setPhonetic(phonetic);
+
+        var kind = response[0].meanings[0].partOfSpeech;
+        setKind(kind);
+        console.log(kind);
+
         console.log(response[0].phonetic);
       })
       .catch((error) => {
         console.log(error);
       });
   };
-  
-
+  const speak = () => {
+    Speech.speak(checkedWord);
+  };
+  const clear = () => {
+    setWord("");
+    setCheckedWord("");
+    setDefinition("");
+    setPhonetic("");
+    setKind("");  
+  };
 
   return (
     <View style={styles.container}>
@@ -50,6 +69,18 @@ export default function App() {
         source={require("./assets/bg.jpg")}
       >
         <View style={styles.contentContainer}>
+          <TouchableOpacity onPress={() => speak()}>
+            <Image
+              source={require("./assets/voice.png")}
+              style={{
+                width: 30,
+                height: 30,
+                marginTop: 220,
+                marginLeft: 8,
+                marginRight: 18,
+              }}
+            />
+          </TouchableOpacity>
           <TextInput
             style={styles.input}
             placeholder="Enter a word"
@@ -58,23 +89,30 @@ export default function App() {
             onChangeText={searchWord}
             value={word}
           />
-          <TouchableOpacity style={styles.searchIconContainer}
-          onPress={requestAPI}
+          <TouchableOpacity
+            style={styles.searchIconContainer}
+            onPress={requestAPI}
           >
             <Image
               source={require("./assets/search.png")}
               style={styles.searchIcon}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.trashIconContainer}>
+          <TouchableOpacity
+            style={styles.trashIconContainer}
+            onPress={() => {
+              clear();
+            }}
+          >
             <Image
               source={require("./assets/trash.png")}
               style={styles.trashIcon}
             />
           </TouchableOpacity>
         </View>
-        <View>
-          <Text style={styles.text}>{definition}</Text>
+        <View style={styles.results}>
+          <Text style={styles.text}>{checkedWord}         {phonetic}</Text>
+          <Text style={styles.text2}>{kind}</Text>
           <Text style={styles.text}>{definition}</Text>
         </View>
       </ImageBackground>
@@ -145,10 +183,25 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
   },
-  text:{
-    fontSize: 30,
+  results: {
+    marginRight: 30,
+    marginLeft: 30,
+    padding: 30,
+    marginBottom: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.8)",
+    borderRadius: 20,
+  },
+  text: {
+    fontSize: 20,
     color: "white",
-    fontWeight: "bold",
-    marginTop: 20,
-  }
+    marginTop: 10,
+  },
+  text2: {
+    fontSize: 20,
+    color: "red",
+    fontFamily: "monospace",
+    marginTop: 10,
+  },
 });
